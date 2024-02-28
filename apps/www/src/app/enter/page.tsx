127c868a465003d8bea06804e9e1372c06efd67a
Unto Death <git@untodeath.jp>
2024-02-28 12:47:00 -0600

    Remove Form from enter

--- apps/www/src/app/enter/page.tsx
+++ apps/www/src/app/enter/page.tsx
@@ -1,12 +1,10 @@
 import { Content } from "./_components/content";
-// import { Form } from "./_components/form";
 import styles from "./page.module.css";
 
 export default function Page() {
   return (
     <main className={styles.root}>
       <Content>
-        {/* <Form /> */}
         <div />
       </Content>
     </main>
