9a9fb05e8186ae76211809736ba3d728e4f7a024
Unto Death <git@untodeath.jp>
2024-02-28 13:39:22 -0600

    Remove root from .eslintrc.js

--- apps/www/.eslintrc.js
+++ apps/www/.eslintrc.js
@@ -1,6 +1,5 @@
 /** @type {import("eslint").Linter.Config} */
 module.exports = {
-  root: true,
   extends: ["@kngsthvs/eslint-config/next.js"],
   parser: "@typescript-eslint/parser",
   parserOptions: {
