c332dc1aa3e5cf64fb18d4de3aea300676711f0f
Unto Death <git@untodeath.jp>
2024-03-14 18:14:46 -0500

    Update partner

--- apps/www/src/app/partner/page.tsx
+++ apps/www/src/app/partner/page.tsx
@@ -1,10 +1,51 @@
-import { Content } from "./_components/content";
+import { Pump } from "basehub/react-pump";
+import { RichText } from "basehub/react-rich-text";
+import { draftMode } from "next/headers";
+import Image from "next/image";
+import Link from "next/link";
 import styles from "./page.module.css";
 
 export default function Page() {
   return (
     <main className={styles.root}>
-      <Content>{null}</Content>
+      <Pump
+        draft={draftMode().isEnabled}
+        next={{ revalidate: 60 }}
+        queries={[
+          {
+            partner: {
+              __typename: true,
+              _title: true,
+              opening: {
+                json: {
+                  content: true,
+                },
+              },
+            },
+          },
+        ]}
+      >
+        {async ([{ partner }]) => {
+          "use server"; // needs to be a Server Action
+
+          return (
+            <div className={styles.content}>
+              <Link href="/">
+                <Image
+                  alt="Kings & Thieves icon"
+                  height={60}
+                  src="/icon.svg"
+                  width={60}
+                />
+              </Link>
+
+              <div>
+                <RichText>{partner.opening?.json.content}</RichText>
+              </div>
+            </div>
+          );
+        }}
+      </Pump>
     </main>
   );
 }
