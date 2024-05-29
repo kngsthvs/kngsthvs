5b9b33471cfb11153be6604bb415cf4e5c5c2516
Unto Death <git@untodeath.jp>
2024-05-28 23:56:34 -0500

    Fix slide

--- apps/www/src/app/investors/[slide]/page.tsx
+++ apps/www/src/app/investors/[slide]/page.tsx
@@ -1,39 +1,38 @@
+import { Pump } from "basehub/react-pump";
+import { draftMode } from "next/headers";
+import styles from "./page.module.css";
+
 export default function Page({ params }: { params: { slide: string[] } }) {
-  // return (
-  //   <Pump
-  //     draft={draftMode().isEnabled}
-  //     next={{ revalidate: 60 }}
-  //     queries={[
-  //       {
-  //         investors: {
-  //           __args: {
-  //             filter: {
-  //               _sys_slug: { eq: params.slide.at(-1) },
-  //             },
-  //           },
-  //           items: {
-  //             _slug: true,
-  //             _title: true,
-  //             content: {
-  //               json: {
-  //                 content: true,
-  //               },
-  //             },
-  //           },
-  //         },
-  //       },
-  //     ]}
-  //   >
-  //     {async ([{ investors }]) => {
-  //       "use server"; // Needs to be a Server Action
-  //       return (
-  //         <div className={styles.root}>
-  //           {/* <p>{apps.items[0]?.status}</p>
-  //       <Image alt="" quality={100} src={icon} />
-  //       <p>{apps.items[0]?.description}</p> */}
-  //         </div>
-  //       );
-  //     }}
-  //   </Pump>
-  // );
+  return (
+    <Pump
+      draft={draftMode().isEnabled}
+      next={{ revalidate: 60 }}
+      queries={[
+        {
+          investors: {
+            __args: {
+              filter: {
+                _sys_slug: { eq: params.slide.at(-1) },
+              },
+            },
+            items: {
+              _slug: true,
+              _title: true,
+              content: {
+                json: {
+                  content: true,
+                },
+              },
+            },
+          },
+        },
+      ]}
+    >
+      {async ([{ investors }]) => {
+        "use server"; // Needs to be a Server Action
+
+        return <div className={styles.root}>{JSON.stringify(investors)}</div>;
+      }}
+    </Pump>
+  );
 }
