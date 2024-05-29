01e7f5158bb5a22c3bed37269b7049ca98dba334
Unto Death <git@untodeath.jp>
2024-05-29 11:54:53 -0500

    Fix investors layout and add content (#6)

--- apps/www/src/app/investors/page.tsx
+++ apps/www/src/app/investors/page.tsx
@@ -4,8 +4,6 @@ import styles from "./page.module.css";
 export default function Page() {
   return (
     <div className={styles.root}>
-      <p>New ETA is 12:00, I spent more time writing this than I expected.</p>
-
       <Cover />
     </div>
   );
